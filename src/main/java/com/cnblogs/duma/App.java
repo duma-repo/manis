efc3ca14d7db5cfd53a257c4b530efcf7d496c09
salmon002 <574361905@qq.com>
2019-05-12 20:33:53 +0800

    init

---
+++ src/main/java/com/cnblogs/duma/App.java
@@ -0,0 +1,13 @@
+package com.cnblogs.duma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
