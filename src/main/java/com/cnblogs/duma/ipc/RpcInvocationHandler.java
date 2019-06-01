99112d4ede3f459cdb47d1a5a7682a37893b6824
duma-repo <zhuimengstudio@163.com>
2019-06-01 23:16:32 +0800

    update get serializable rpc proxy

---
+++ src/main/java/com/cnblogs/duma/ipc/RpcInvocationHandler.java
@@ -0,0 +1,6 @@
+package com.cnblogs.duma.ipc;
+
+import java.lang.reflect.InvocationHandler;
+
+public interface RpcInvocationHandler extends InvocationHandler {
+}
--- src/main/java/com/cnblogs/duma/ipc/SerializableRpcEngine.java
+++ src/main/java/com/cnblogs/duma/ipc/SerializableRpcEngine.java
@@ -1,14 +1,41 @@
 package com.cnblogs.duma.ipc;
 
 import com.cnblogs.duma.conf.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.net.SocketFactory;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
 
 public class SerializableRpcEngine implements RpcEngine {
+    public static final Log LOG = LogFactory.getLog(SerializableRpcEngine.class);
+
     @Override
-    public <T> T getProxy(Class<T> protocol, long clientVersion, InetSocketAddress address, Configuration conf, SocketFactory factory, int rpcTimeOut) throws IOException {
-        return null;
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy(Class<T> protocol, long clientVersion,
+                          InetSocketAddress address, Configuration conf,
+                          SocketFactory factory, int rpcTimeOut)
+            throws IOException {
+
+        final Invoker invoker = new Invoker(protocol, address, conf, factory, rpcTimeOut);
+        return (T) Proxy.newProxyInstance(protocol.getClassLoader(), new Class[]{protocol}, invoker);
+    }
+
+    private static class Invoker implements RpcInvocationHandler {
+
+        Invoker(Class<?> protocol, InetSocketAddress address,
+                Configuration conf, SocketFactory factory,
+                int rpcTimeOut)
+                throws IOException {
+            System.out.println("init Invoker");
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return null;
+        }
     }
 }
