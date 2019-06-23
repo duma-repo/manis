246346f459ecccc7f07e1be8f3479d14eda58865
duma-repo <zhuimengstudio@163.com>
2019-06-23 16:49:06 +0800

    add ConnectionId Class

--- src/main/java/com/cnblogs/duma/ipc/Client.java
+++ src/main/java/com/cnblogs/duma/ipc/Client.java
@@ -1,10 +1,13 @@
 package com.cnblogs.duma.ipc;
 
+import com.cnblogs.duma.conf.CommonConfigurationKeysPublic;
 import com.cnblogs.duma.conf.Configuration;
 import com.cnblogs.duma.io.Writable;
 
 import javax.net.SocketFactory;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Objects;
 
 public class Client {
     private Class<? extends Writable> valueClass;
@@ -23,8 +26,113 @@ public class Client {
         return null;
     }
 
-
+    /**
+     *  该类用来存储与连接相关的 address、protocol 等信息
+     *  todo 1. 需要 getConnectionId
+     *      2. 重试机制
+     *      3. 解释hashcode
+     */
     public static class ConnectionId {
+        final InetSocketAddress address;
+        private static final int PRIME = 16777619;
+        private final Class<?> protocol;
+        private final int rpcTimeOut;
+        /** 连接空闲时最大休眠时间，单位：毫秒 */
+        private final int maxIdleTime;
+        /** 如果 true 表示禁用 Nagle 算法 */
+        private final boolean tcpNoDelay;
+        /** 是否需要发送ping message */
+        private final boolean doPing;
+        /** 发送 ping message 的时间间隔， 单位：毫秒 */
+        private final int pingInterval;
+        private final Configuration conf;
 
+        public ConnectionId(InetSocketAddress address,
+                            Class<?> protocol,
+                            int rpcTimeOut,
+                            Configuration conf) {
+            this.address = address;
+            this.protocol = protocol;
+            this.rpcTimeOut = rpcTimeOut;
+
+            this.maxIdleTime = conf.getInt(
+                    CommonConfigurationKeysPublic.IPC_CLIENT_CONNECTION_MAXIDLETIME_KEY,
+                    CommonConfigurationKeysPublic.IPC_CLIENT_CONNECTION_MAXIDLETIME_DEFAULT);
+            this.tcpNoDelay = conf.getBoolean(
+                    CommonConfigurationKeysPublic.IPC_CLIENT_TCPNODELAY_KEY,
+                    CommonConfigurationKeysPublic.IPC_CLIENT_TCPNODELAY_DEFAULT);
+            this.doPing = conf.getBoolean(
+                    CommonConfigurationKeysPublic.IPC_CLIENT_PING_KEY,
+                    CommonConfigurationKeysPublic.IPC_CLIENT_PING_DEFAULT);
+            this.pingInterval = doPing ?
+                    conf.getInt(
+                            CommonConfigurationKeysPublic.IPC_PING_INTERVAL_KEY,
+                            CommonConfigurationKeysPublic.IPC_PING_INTERVAL_DEFAULT)
+                    : 0;
+            this.conf = conf;
+        }
+
+        public InetSocketAddress getAddress() {
+            return address;
+        }
+
+        public Class<?> getProtocol() {
+            return protocol;
+        }
+
+        public int getRpcTimeOut() {
+            return rpcTimeOut;
+        }
+
+        public int getMaxIdleTime() {
+            return maxIdleTime;
+        }
+
+        public boolean isTcpNoDelay() {
+            return tcpNoDelay;
+        }
+
+        public boolean isDoPing() {
+            return doPing;
+        }
+
+        public int getPingInterval() {
+            return pingInterval;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj instanceof ConnectionId) {
+                ConnectionId that = (ConnectionId) obj;
+                return Objects.equals(this.address, that.address)
+                        && Objects.equals(this.protocol, that.protocol)
+                        && this.rpcTimeOut == that.rpcTimeOut
+                        && this.maxIdleTime == that.maxIdleTime
+                        && this.tcpNoDelay == that.tcpNoDelay
+                        && this.doPing == that.doPing
+                        && this.pingInterval == that.pingInterval;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = ((address == null) ? 0 : address.hashCode());
+            result = PRIME * result + (doPing ? 1231 : 1237);
+            result = PRIME * result + maxIdleTime;
+            result = PRIME * result + pingInterval;
+            result = PRIME * result + ((protocol == null) ? 0 : protocol.hashCode());
+            result = PRIME * result + rpcTimeOut;
+            result = PRIME * result + (tcpNoDelay ? 1231 : 1237);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return address.toString();
+        }
     }
 }
