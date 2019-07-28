bb545ec232493082fa7de46b811a3fd0f9fe57ed
duma-repo <zhuimengstudio@163.com>
2019-07-28 21:46:30 +0800

    add StringUtils tool class

--- src/main/java/com/cnblogs/duma/util/StringUtils.java
+++ src/main/java/com/cnblogs/duma/util/StringUtils.java
@@ -1,4 +1,15 @@
 package com.cnblogs.duma.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class StringUtils {
+
+    public static String stringifyException(Throwable e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(sw);
+        e.printStackTrace(printWriter);
+        printWriter.close();
+        return sw.toString();
+    }
 }
