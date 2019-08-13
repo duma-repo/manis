79601d4ac717a6c09e58758822b2693761f49903
duma-repo <zhuimengstudio@163.com>
2019-08-13 23:20:28 +0800

    add property and constructor

--- src/main/java/com/cnblogs/duma/io/ObjectWritable.java
+++ src/main/java/com/cnblogs/duma/io/ObjectWritable.java
@@ -5,6 +5,17 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class ObjectWritable implements Writable {
+
+    private Class declaredClass;
+    private Object instance;
+
+    public ObjectWritable() {}
+
+    public ObjectWritable(Class declaredClass, Object instance) {
+        this.declaredClass = declaredClass;
+        this.instance = instance;
+    }
+
     @Override
     public void write(DataOutput out) throws IOException {
 
