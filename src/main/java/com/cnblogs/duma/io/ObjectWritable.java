34c405b0c9780316afccbd1329dff1993a9c46cd
duma-repo <zhuimengstudio@163.com>
2019-08-14 22:57:06 +0800

    complete ObjectWritable class

--- src/main/java/com/cnblogs/duma/io/ObjectWritable.java
+++ src/main/java/com/cnblogs/duma/io/ObjectWritable.java
@@ -1,8 +1,6 @@
 package com.cnblogs.duma.io;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.*;
 
 public class ObjectWritable implements Writable {
 
@@ -16,13 +14,43 @@ public class ObjectWritable implements Writable {
         this.instance = instance;
     }
 
+    public Object get() {
+        return instance;
+    }
+
     @Override
     public void write(DataOutput out) throws IOException {
+        ByteArrayOutputStream byteArrOut = new ByteArrayOutputStream();
+        ObjectOutputStream objOut = new ObjectOutputStream(byteArrOut);
 
+        objOut.writeObject(declaredClass);
+        objOut.writeObject(instance);
+        objOut.flush();
+
+        byte[] data = byteArrOut.toByteArray();
+        out.writeInt(data.length);
+        out.write(data);
     }
 
     @Override
     public void readFields(DataInput in) throws IOException {
+        int length = in.readInt();
+        byte[] data = new byte[length];
+        in.readFully(data);
 
+        ByteArrayInputStream byteArrIn = new ByteArrayInputStream(data);
+        ObjectInputStream objIn = new ObjectInputStream(byteArrIn);
+
+        try {
+            declaredClass = (Class) objIn.readObject();
+            instance = objIn.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Class not found when deserialize.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "OW[class=" + declaredClass + ",value=" + instance + "]";
     }
 }
